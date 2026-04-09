134f9b31bf250b174a2f7f68e3aec51b36e5f333
AdamLaptop <adamsecond94@gmail.com>
2026-04-08 20:14:19 -0500

    feat: add event listener and send alert

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-function init(){
-//add your javascrip between these two lines of code
+function init() {
+  const alertButton = document.getElementById('entrybutton');
+  const textInput = document.getElementById('entryinput');
+  const h2Element = document.getElementById('textoutput');
+
+  alertButton.addEventListener('click', function() {
+    const inputValue = textInput.value;
+    alert('Adam Jaramillo: ' + inputValue);
+    h2Element.textContent = inputValue;
+  });
 }
-
-
-
-
-
-
 
 window.addEventListener('load', init);
