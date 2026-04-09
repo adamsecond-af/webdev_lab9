e486ab48b021f1e41e7b860c547e6af3ff13a894
AdamLaptop <adamsecond94@gmail.com>
2026-04-08 20:16:50 -0500

    feat: add fallback for no input

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ function init() {
 
   alertButton.addEventListener('click', function() {
     const inputValue = textInput.value;
+    if(!inputValue){
+      alert('Adam Jaramillo: There was no inputted value');
+      h2Element.textContent = "No value was inputted"
+      return;
+    }
+
     alert('Adam Jaramillo: ' + inputValue);
     h2Element.textContent = inputValue;
   });
